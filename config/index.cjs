b66fc7ba5ee77fc98256440e34f6d3ad359e9d2e
mshlxm <144439387+mshlxm@users.noreply.github.com>
2023-09-11 16:12:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -45,7 +45,9 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-7-10' },
+        { keyword: 'love_day', date: '2023-7-19' },
+          // 看熊猫
+        { keyword: 'lovee_day', date: '2023-7-15' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2025-10-1' },
       ],
