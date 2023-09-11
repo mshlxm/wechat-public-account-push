73ca869bec9ae1654c556845f29def32ffc48a9f
mshlxm <144439387+mshlxm@users.noreply.github.com>
2023-09-11 10:38:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: ['oTZ3v6rn9HnoIbDB3L4Ih1gSICC4'],
+      id: 'oTZ3v6rn9HnoIbDB3L4Ih1gSICC4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'WNtEJzwruGw1zBXAXxQ264QfkpUoUWZ4SkDfiYR-F_U',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -60,7 +60,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: ['oTZ3v6rn9HnoIbDB3L4Ih1gSICC4'],
+      id: 'oTZ3v6rn9HnoIbDB3L4Ih1gSICC4',
     }
   ],
 
