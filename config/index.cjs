ab9b90e48a24dfdf19ee02afca10794899fda2e0
mshlxm <144439387+mshlxm@users.noreply.github.com>
2023-09-11 17:27:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -35,12 +35,12 @@ const USER_CONFIG = {
           type: '*生日', name: '宝贝', year: '1998', date: '10-8',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '2002', date: '12-25',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '7-10',
-        },
+        //{
+       //   type: '生日', name: '李四', year: '2002', date: '12-25',
+      //  },
+     //   {
+      //    type: '节日', name: '相识纪念日', year: '2023', date: '7-10',
+      //  },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
